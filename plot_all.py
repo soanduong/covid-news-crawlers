f455fe278a744876eb012f869b30ba1329ef0666
soanduong <soandm@gmail.com>
2021-03-22 16:03:01 +1100

    - Polish the plotting. - Add the crawled data of the other sites.

--- plot_all.py
+++ plot_all.py
@@ -24,11 +24,12 @@ from scipy.signal import savgol_filter
 from tqdm import tqdm
 
 # Set the site names
-site_names = ['laodong', '24h', 'kenh14', 'dantri', 'cafef', 'vnexpress']
+site_names = ['laodong', '24h', 'kenh14', 'dantri', 'cafef',
+              'vnexpress', 'tuoitre', 'vietnamnet', 'zingnews', 'thanhnien']
 
 # Set the date range
-date_start = '2019-11-17'
-date_end = '2021-01-01'
+date_start = '2020-01-01'
+date_end = '2020-12-31'
 date_list = pd.date_range(date_start, date_end).tolist()
 # date_list_display = [item.strftime('%d/%m/%y') for item in date_list]
 # date_list_display = [item.strftime('%d/%m') for item in date_list]
@@ -67,12 +68,13 @@ plt.plot(date_list, n_articles_list, 'lightsalmon', linewidth=linewidth,
 plt.plot(date_list, n_articles_smooth, 'r', linewidth=linewidth,
          label='Third-order smoothness')
 plt.legend()
-plt.xlabel('Timeline')
+# plt.xlabel('Timeline')
 plt.ylabel('No. news')
 plt.grid(True)
 fig.tight_layout()
-plt.xticks([date_list[0], date_list[109], date_list[158],
-            date_list[249], date_list[284], date_list[-1]],
+# plt.xlabel('Timeline') date_list[63],
+plt.xticks([date_list[0], date_list[22], date_list[113],
+            date_list[204], date_list[224], date_list[-1]],
            rotation=rotation)
 plt.xlim([date_list[0], date_list[-1]])
 plt.savefig(f'figs/plot_articles.png', dpi=300, bbox_inches='tight')
@@ -88,44 +90,10 @@ plt.plot(date_list, grad_n_articles, 'dodgerblue', linewidth=2)
 plt.ylabel('Gradient of the smoothness')
 
 plt.grid(True)
-plt.xlabel('Timeline')
-plt.xticks([date_list[0], date_list[109], date_list[158],
-            date_list[249], date_list[284], date_list[-1]],
+# plt.xlabel('Timeline') date_list[63],
+plt.xticks([date_list[0], date_list[22], date_list[113],
+            date_list[204], date_list[224], date_list[-1]],
            rotation=rotation)
 plt.xlim([date_list[0], date_list[-1]])
 plt.savefig(f'figs/plot_gradient.png', dpi=300, bbox_inches='tight')
-plt.show()
-
-
-# Display the all three curve (actual, smooth, and gradient)
-plt.close()
-fig = plt.figure(2, fig_size)
-ax1 = plt.gca()
-plt.rcParams.update({'font.size': fontsize})
-plt.rc('grid', linestyle="--", color='lightgray')
-ax1.plot(date_list, n_articles_list, 'lightsalmon',
-         linewidth=linewidth, linestyle='--', label='Actual numbers')
-ax1.plot(date_list, n_articles_smooth, 'r', linewidth=linewidth,
-         label='Third-order smoothness')
-
-# plt.legend()
-plt.xticks([date_list[0], date_list[109], date_list[158],
-            date_list[249], date_list[284], date_list[-1]],
-           rotation=rotation)
-ax2 = ax1.twinx()
-ax2.plot(date_list, grad_n_articles, 'dodgerblue',
-         linewidth=2, label='Gradient of smoothness')
-
-# plt.legend()
-ax1.grid()
-plt.xticks([date_list[0], date_list[109], date_list[158],
-            date_list[249], date_list[284], date_list[-1]],
-           rotation=rotation)
-ax1.set_xlabel('Timeline')
-ax1.set_ylabel('No. news', color='tab:red')
-ax2.set_ylabel('Gradient of the smoothness', color='dodgerblue')
-ax1.set_xlim([date_list[0], date_list[-1]])
-fig.legend(loc="upper center", bbox_to_anchor=(0.45, 1.03),
-           ncol=3, fancybox=True)
-plt.savefig(f'figs/plot_all.png', dpi=300, bbox_inches='tight')
 plt.show()
